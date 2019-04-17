2fba9998b03cc4f4a2f551f383acccdd37b80462
Caption-He <912772051@qq.com>
2019-04-17 14:38:46 +0800

    4 17

--- pages/components/xing-editor.js
+++ pages/components/xing-editor.js
@@ -188,8 +188,21 @@ Component({
       wx.showLoading({
         title: '正在保存',
       })
-      this.writeTextToNode();
-      this.handleOutput();
+      wx.request({
+        url: 'https://www.caption-he.com.cn/xcx/hone/index/updatetxt', // 仅为示例，并非真实的接口地址
+        data: {
+          x: '',
+          y: ''
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success(res) {
+          console.log(res.data)
+        }
+      })
+     // this.writeTextToNode();
+      //this.handleOutput();
     },
 
     /**
